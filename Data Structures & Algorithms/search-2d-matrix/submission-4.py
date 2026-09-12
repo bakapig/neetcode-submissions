fce817f62a42f39ea1ee8314af6a813bdeb4081c
Chin <39308296+bakapig@users.noreply.github.com>
2026-09-12 22:32:19 +0800

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        from bisect import bisect_left
+
+        res = []
+
+        for lst in matrix:
+            for item in lst:
+                res.append(item)
+
+        idx = bisect_left(res, target)
+        # idx >= len(nums) or target != nums[idx]
+        if idx < len(res) and res[idx] == target:
+            return True
+        else:
+            return False
